457ce35509aa64ddfd6f76f655101ca7db7cf725
Fennah267 <cherekaf@btc.bw>
2025-04-16 16:43:07 +0200

    updating the hive js file

--- server-main/services/hive.js
+++ server-main/services/hive.js
@@ -15,7 +15,7 @@ const getSmegaStatement = async (query) => {
         const principal = process.env.PRINCIPAL;
 
         console.log("Running kinit...");
-        execSync(`kinit -kt ${keytabPath} ${principal}`);
+        execSync(`sudo kinit -kt ${keytabPath} ${principal}`);
         console.log("kinit successful");
 
         // Step 2: Connect to Hive with kerberos
