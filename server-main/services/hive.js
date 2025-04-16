4ac496841565ef628e44254a9bc43089b3c63557
Fennah267 <cherekaf@btc.bw>
2025-04-16 12:41:45 +0200

    updating the env and hive.js file for kebros security

--- server-main/services/hive.js
+++ server-main/services/hive.js
@@ -1,6 +1,57 @@
 import hive from "hive-driver";
 import { config } from "dotenv";
 
+config();
+
+const { TCLIService, TCLIService_types } = hive.thrift;
+const client = new hive.HiveClient(TCLIService, TCLIService_types);
+const utils = new hive.HiveUtils(TCLIService_types);
+
+const getSmegaStatement = async (query) => {
+    try {
+        await client.connect(
+            {
+                host: process.env.HIVE_HOST,
+                port: parseInt(process.env.HIVE_PORT),
+                options: {
+                    principal: process.env.PRINCIPAL,
+                    kerberosServiceName: 'hive', 
+                    timeout: parseInt(process.env.CONNECTION_TIMEOUT)
+                }
+            },
+            new hive.connections.TcpConnection(),
+            new hive.auth.KerberosTcpAuthentication({
+                keytabFile: process.env.KEYTAB_HOME,
+                principal: process.env.PRINCIPAL
+            })
+        );
+
+        const session = await client.openSession({
+            client_protocol: TCLIService_types.TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V10
+        });
+
+        const operation = await session.executeStatement(query);
+        await utils.waitUntilReady(operation, false, () => {});
+        const selectDataOperation = await utils.fetchAll(operation);
+        const results = utils.getResult(selectDataOperation).getValue();
+
+        await operation.close();
+        await session.close();
+
+        return results;
+    } catch (err) {
+        console.error("Error in getSmegaStatement:", err);
+        throw err;
+    }
+};
+
+export { getSmegaStatement };
+
+
+
+/*import hive from "hive-driver";
+import { config } from "dotenv";
+
 
 config();
 
@@ -41,4 +92,5 @@ const getSmegaStatement = async (query) => {
     }
 };
 
-export { getSmegaStatement };
+export { getSmegaStatement };*/
+
